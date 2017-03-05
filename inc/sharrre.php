942e722db267d8fe6b55fb67e88bb654f44e84be
Alexander Agnarson <alexander.agnarson@gmail.com>
2017-03-05 20:53:56 +0100

    Add sharrre language strings

--- inc/sharrre.php
+++ inc/sharrre.php
@@ -1,9 +1,9 @@
 <div class="sharrre-container">
 	<span><?php esc_html_e('Share','typecore'); ?></span>
-	<div id="twitter" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="Tweet"></div>
-	<div id="facebook" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="Like"></div>
-	<div id="googleplus" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="+1"></div>
-	<div id="pinterest" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="Pin It"></div>
+	<div id="twitter" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="<?php esc_html_e('Tweet', 'typecore'); ?>"></div>
+	<div id="facebook" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="<?php esc_html_e('Like', 'typecore'); ?>"></div>
+	<div id="googleplus" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="<?php esc_html_e('+1', 'typecore'); ?>"></div>
+	<div id="pinterest" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-title="<?php esc_html_e('Pin It', 'typecore'); ?>"></div>
 </div><!--/.sharrre-container-->
 
 <script type="text/javascript">
