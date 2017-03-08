9535ff504c1aedc846551bbde0aff9dfa0ef7788
Alexander Agnarson <alexander.agnarson@gmail.com>
2017-03-08 16:52:07 +0100

    Add missing esc_url

--- footer.php
+++ footer.php
@@ -79,7 +79,7 @@
 						
 						<?php if ( ot_get_option( 'credit' ) != 'off' ): ?>
 						<div id="credit">
-							<p><?php esc_html_e('Powered by','typecore'); ?> <a href="http://wordpress.org" rel="nofollow">WordPress</a>. <?php esc_html_e('Theme by','typecore'); ?> <a href="http://alxmedia.se" rel="nofollow">Alx</a>.</p>
+							<p><?php esc_html_e('Powered by','typecore'); ?> <a href="<?php echo esc_url( 'http://wordpress.org' ); ?>" rel="nofollow">WordPress</a>. <?php esc_html_e('Theme by','typecore'); ?> <a href="http://alxmedia.se" rel="nofollow">Alx</a>.</p>
 						</div><!--/#credit-->
 						<?php endif; ?>
 						
