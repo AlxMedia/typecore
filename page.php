255d19f348d37bf52c582818966d23b3aff71471
Alexander Agnarson <alexander.agnarson@gmail.com>
2018-07-20 13:04:50 +0200

    Page/post comments options removed as it is already in core

--- page.php
+++ page.php
@@ -19,7 +19,7 @@
 				
 			</article>
 			
-			<?php if ( get_theme_mod( 'page-comments', 'off' ) == 'on' ) { comments_template('/comments.php',true); } ?>
+			<?php if ( comments_open() || get_comments_number() ) :	comments_template( '/comments.php', true ); endif; ?>
 			
 		<?php endwhile; ?>
 		
