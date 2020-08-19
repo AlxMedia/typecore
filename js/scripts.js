606c3ad15cf52903cd641363934202728e578ddc
Alexander Agnarson <alexander.agnarson@gmail.com>
2020-08-19 11:41:19 +0200

    Remove unused js

--- js/scripts.js
+++ js/scripts.js
@@ -87,18 +87,6 @@ jQuery(document).ready(function($) {
 			$('body').toggleClass('s1-expand').toggleClass('s1-collapse');
 		}
 	});
-
-/*  Dropdown menu animation
-/* ------------------------------------ */
-	$('.nav ul.sub-menu').hide();
-	$('.nav li').hover( 
-		function() {
-			$(this).children('ul.sub-menu').slideDown('fast');
-		}, 
-		function() {
-			$(this).children('ul.sub-menu').hide();
-		}
-	);
 	
 /*  Fitvids
 /* ------------------------------------ */
@@ -109,38 +97,5 @@ jQuery(document).ready(function($) {
 		}
 		
 	responsiveVideo();
-	
-/*  Mobile menu smooth toggle height
-/* ------------------------------------ */	
-	$('.nav-toggle').on('click', function() {
-		slide($('.nav-wrap .nav', $(this).parent()));
-	});
-	 
-	function slide(content) {
-		var wrapper = content.parent();
-		var contentHeight = content.outerHeight(true);
-		var wrapperHeight = wrapper.height();
-	 
-		wrapper.toggleClass('expand');
-		if (wrapper.hasClass('expand')) {
-		setTimeout(function() {
-			wrapper.addClass('transition').css('height', contentHeight);
-		}, 10);
-	}
-	else {
-		setTimeout(function() {
-			wrapper.css('height', wrapperHeight);
-			setTimeout(function() {
-			wrapper.addClass('transition').css('height', 0);
-			}, 10);
-		}, 10);
-	}
-	 
-	wrapper.one('transitionEnd webkitTransitionEnd transitionend oTransitionEnd msTransitionEnd', function() {
-		if(wrapper.hasClass('open')) {
-			wrapper.removeClass('transition').css('height', 'auto');
-		}
-	});
-	}
 	
 });
